f81a275df5e14f6d82afe6a2ac773db9fdf350d1
Raiden Anderson <randerson462@stkentigernsacademy.org.uk>
2016-06-27 02:33:09 -0700

    Version 1

---
+++ GUESS.java
@@ -0,0 +1,29 @@
+
+public class GUESS
+{
+    private String playerName;
+    private String gameChoice;
+    private int randomNumber;
+    private int guess;
+    Random randomGenerator = new Random();
+    /**
+     * Constructor for objects of class GUESS
+     */
+    public GUESS()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
